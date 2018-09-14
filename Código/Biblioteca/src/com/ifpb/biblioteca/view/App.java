04c1fca76cbba939f9cf68a0f7f927b6cbcaef89
camiula <camilacarwalho3@gmail.com>
2018-09-13 21:57:13 -0300

    Faltam terminar o menu e crud de devolução

--- Código/Biblioteca/src/com/ifpb/biblioteca/view/App.java
+++ Código/Biblioteca/src/com/ifpb/biblioteca/view/App.java
@@ -1,9 +1,6 @@
 package com.ifpb.biblioteca.view;
 
-import com.ifpb.biblioteca.control.CadastroAutor;
-import com.ifpb.biblioteca.control.CadastroCliente;
-import com.ifpb.biblioteca.control.CadastroEmprestimos;
-import com.ifpb.biblioteca.control.CadastroLivro;
+import com.ifpb.biblioteca.control.*;
 import com.ifpb.biblioteca.model.*;
 //Menu de teste incompleto
 import java.time.LocalDate;
@@ -16,126 +13,142 @@ public class App {
         CadastroCliente crudCliente = new CadastroCliente();
         CadastroEmprestimos crudEmprestimo = new CadastroEmprestimos();
         CadastroLivro crudLivro = new CadastroLivro();
+        CadastroFuncionario crudFuncionario = new CadastroFuncionario();
+        ShowText menu = new ShowText();
 
-//        Autor stephen = new Autor("Stephen Hawking",23);
-//        Autor edgar = new Autor("Edgar Allan Poe",34);
-//        Livro livros[] ={
-//               new Livro("O gato preto",12,Genero.TERROR,edgar,"Poe conta a história de um homem que, movido pelo abuso de álcool e transtornado pelo amor incondicional que um bicho pode dedicar a seu dono, acaba por enforcar seu próprio gato de estimação. Perseguido pelo fantasma do animal, ele adota outro gato, que, com o passar do tempo, além de despertar a mesma aversão em seu dono, revela, em sua pelagem, a marca da forca."),
-//               new Livro("Uma Breve História do Tempo",2,Genero.DIDATICO,stephen,"Uma Breve História do Tempo: do Big Bang aos Buracos Negros, é um livro de divulgação científica escrito pelo Professor Stephen Hawking, publicado pela primeira vez em 1988")
-//
-//
-//        };
-//        Cliente cliente = new Cliente("Teo",23421,"111.111.111-11","teofanes@email.com","camiulaehlinda123");
-//        boolean continua = true;
-//        int opcao;
-//        Scanner scan = new Scanner(System.in);
-//        while(continua){
-//
-//            System.out.println("1: Adicionar livro");
-//            System.out.println("2: Editar livro");
-//            System.out.println("3: Adicionar cliente");
-//            System.out.println("4: Editar cliente");
-//            System.out.println("5: Emprestar livro");
-//            System.out.println("6: Ver livros");
-//            System.out.println("7: Devolver livro");
-//            System.out.println("0: Encerrar programa");
-//
-//            opcao = scan.nextInt();
-//
-//            if(opcao<0){
-//                System.out.println("ERROR: ENTER para continuar");
-//                scan.nextLine();
-//            }
-//            else{
-//
-//                switch(opcao){
-//                    case 1:System.out.println("Cadastro de Livro:");
-//                        System.out.println("Título:");
-//                        String titulo = scan.next();
-//                        System.out.println("Código:");
-//                        int codigo = scan.nextInt();
-//                        System.out.println("Gênero \n 1:ROMANCE \n 2:TERROR \n 3:POESIA\n4:FANTASIA\n5:AVENTURA");
-//                        int genero = scan.nextInt();
-//                        System.out.println("Autor:");
-//                        String autor = scan.next();
-//                        System.out.println("Descrição");
-//                        String descricao = scan.next();
-//                        Livro livroNovo = new Livro(titulo,codigo,Genero.ROMANCE,edgar,descricao);
-//                        System.out.println("LIVRO CADASTRADO COM SUCESSO");
-//                        break;
-//                    case 2: System.out.println("Qual livro deseja editar?");
-//
-//                            System.out.println(livros[0].getCodigo()+":"+livros[0]);
-//                            System.out.println(livros[1].getCodigo()+":"+livros[1]);
-//                            System.out.println("Digite o código:");
-//                            int codigoEditar = scan.nextInt();
-//                            switch (codigoEditar){
-//                                case 12:
-//                                    System.out.println("Título:");
-//                                    String tituloNovo = scan.next();
-//                                    System.out.println("Código:");
-//                                    int codigoNovo = scan.nextInt();
-//                                    System.out.println("Gênero \n 1:ROMANCE \n 2:TERROR \n 3:POESIA\n4:FANTASIA\n5:AVENTURA");
-//                                    int generoNovo = scan.nextInt();
-//                                    System.out.println("Autor:");
-//                                    String autorNovo= scan.next();
-//                                    System.out.println("Descrição");
-//                                    String descricaoNovo = scan.next();
-//                                    livros[0]= new Livro(tituloNovo,codigoNovo,Genero.ROMANCE,edgar,descricaoNovo);
-//                                    System.out.println("LIVRO EDITADO COM SUCESSO");
-//                                break;
-//                                case 2:
-//                                    System.out.println("Título:");
-//                                    String tituloNovo2 = scan.next();
-//                                    System.out.println("Código:");
-//                                    int codigoNovo2 = scan.nextInt();
-//                                    System.out.println("Gênero \n 1:ROMANCE \n 2:TERROR \n 3:POESIA\n4:FANTASIA\n5:AVENTURA");
-//                                    int generoNovo2 = scan.nextInt();
-//                                    System.out.println("Autor:");
-//                                    String autorNovo2= scan.next();
-//                                    System.out.println("Descrição");
-//                                    String descricaoNovo2 = scan.next();
-//                                    livros[1]= new Livro(tituloNovo2,codigoNovo2,Genero.ROMANCE,edgar,descricaoNovo2);
-//                                    System.out.println("LIVRO EDITADO COM SUCESSO");
-//                                break;
-//                            }
-//                        break;
-//                    case 3: System.out.println("Cadastro de Cliente");
-//                        System.out.println("Nome:");
-//                        String nome = scan.next();
-//                        System.out.println("Matricula");
-//                        int matricula = scan.nextInt();
-//                        System.out.println("CPF");
-//                        String cpf = scan.next();
-//                        System.out.println("E-mail:");
-//                        String email = scan.next();
-//                        System.out.println("Senha:");
-//                        String senha = scan.next();
-//                        Cliente clienteNovo = new Cliente(nome,matricula,cpf,email,senha);
-//                        System.out.println("CLIENTE CADASTRADO COM SUCESSO");
-//                        break;
-//                    case 4:
-//                        System.out.println("Que cliente deseja editar?");
-//                        break;
-//                    case 5: System.out.println("Informe titulo do livro que deseja emprestar");
-//                        break;
-//                    case 6: System.out.println("Livros:");
-//                        System.out.println(livros[0]);
-//                        System.out.println(livros[1]);
-//
-//                        break;
-//                    case 7: System.out.println("Informe título do livro que deseja devolver:");
-//
-//                        break;
-//
-//                    case 0: continua = false;
-//                }
-//            }
-//            limpaTela();
-//        }
-//        System.out.println("--------------------------------------");
+
+        boolean continua = true;
+        int seleciona;
+        int selecionaMenu;
+        Scanner scan = new Scanner(System.in);
+
+
+        while(continua){
+            System.out.println(":.:.:.:.TELA INICIAL.:.:.:.:\n\n");
+            System.out.println("1: Autenticar");
+            System.out.println("2: Criar nova conta:");
+            seleciona = scan.nextInt();
+
+            if(seleciona<0||seleciona>2){
+                System.out.println("ERROR:Press ENTER para continar!");
+                scan.nextLine();
+            }
+            else{
+                switch(seleciona){
+                    case 1:
+                        if(menu.login(crudFuncionario)){
+                            System.out.println("Login feito com sucesso!!!");
+                            boolean continuaMenu = true;
+                            while(continuaMenu) {
+                                menu.menuFuncionario();
+                                selecionaMenu = scan.nextInt();
+                                if(selecionaMenu<1||selecionaMenu>6){
+                                    System.out.println("ERROR:Press ENTER para continar!");
+                                    scan.nextLine();
+                                }else {
+                                    int selecionaFunc;
+                                    switch(selecionaMenu){
+                                        case 1:
+                                            System.out.println(crudLivro);
+                                            break;
+                                        case 2: menu.readCliente(crudCliente);
+                                            break;
+                                        case 3:  System.out.println("Digite novamente o seu email:");
+                                                 String email = scan.next();
+                                                 System.out.println("Digite a sua senha\n");
+                                                 String senha = scan.next();
+                                                 crudFuncionario.deleteThis(email, senha);
+                                                 System.out.println("Funcionário deletado com sucesso!\n");
+                                                 continuaMenu=false;
+                                            break;
+                                        case 4:
+                                            boolean continuaM = true;
+                                            int index;
+                                            while(continuaMenu){
+                                                menu.gerenciaEmprestimo();
+                                                int selecionaM = scan.nextInt();
+                                                if(selecionaM<1||selecionaM>6){
+                                                    System.out.println("ERROR:Press ENTER para continar!");
+                                                    scan.nextLine();
+                                                }
+                                                else{
+                                                    switch(selecionaM){
+                                                        case 1:
+                                                            System.out.println(crudEmprestimo);
+                                                            break;
+                                                        case 2:
+                                                            break;
+                                                        case 3: menu.doEmprestimo(crudLivro, crudCliente, crudEmprestimo);
+                                                            break;
+                                                        case 4:
+                                                         continuaM = false;
+                                                            break;
+
+                                                    }
+                                                }
+                                            }
+
+                                            break;
+                                        case 5:  boolean continuaL = true;
+                                            int indexL;
+                                            while(continuaL){
+                                                menu.gerenciaLivros();
+                                                int selecionaM = scan.nextInt();
+                                                if(selecionaM<1||selecionaM>5){
+                                                    System.out.println("ERROR:Press ENTER para continar!");
+                                                    scan.nextLine();
+                                                }
+                                                else{
+                                                    switch(selecionaM){
+                                                        case 1:
+                                                            menu.readLivro(crudLivro);
+                                                            break;
+                                                        case 2:
+                                                            System.out.println("Selecione qual livro deseja deletar:");
+                                                            System.out.println(crudLivro);
+                                                            int deletar = scan.nextInt();
+                                                            crudLivro.delete(deletar-1);
+                                                            System.out.println("Livro deletado com sucesso!!!");
+                                                            break;
+                                                        case 3:
+                                                            System.out.println("Selecione qual livro deseja editar:");
+                                                            System.out.println(crudLivro);
+                                                            int editar = scan.nextInt();
+
+                                                            //crudLivro.update(editar, );
+                                                            break;
+                                                        case 4:
+                                                            continuaM = false;
+                                                            break;
+
+                                                    }
+                                                }
+                                            }
+                                            break;
+                                        case 6: continuaMenu = false;
+                                            break;
+                                    }
+                                }
+
+                            }
+                        }else System.out.println("Funcionário não cadastrado!!!");
+
+                        break;
+                    case 2: menu.readFuncionario(crudFuncionario);
+
+                        break;
+
+                    case 0: continua = false;
+                }
+            }
+            limpaTela();
+        }
+        System.out.println("::.:.:.:.:.:FIM::.:.:.:.:.:");
+
 
     }
+
+
     public static void limpaTela(){
         for (int i=0; i<30;i++) System.out.println("\n");
 
--- Código/Biblioteca/src/com/ifpb/biblioteca/view/ShowText.java
+++ Código/Biblioteca/src/com/ifpb/biblioteca/view/ShowText.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class ShowText {
 
-    public static boolean login(CadastroFuncionario crudF){
+    public static boolean login(CadastroFuncionario crudF) {
         Scanner scan = new Scanner(System.in);
         System.out.println(":.:.:.:.:.LOGIN.:.:.:.:.:\n\n");
         System.out.println("        FUNCIONARIO       \n");
@@ -17,10 +17,10 @@ public class ShowText {
         String email = scan.next();
         System.out.println("Informe sua senha:");
         String senha = scan.next();
-        return crudF.autentication(email,senha);
+        return crudF.autentication(email, senha);
     }
 
-    public static boolean loginCliente(CadastroCliente crudC){
+    public static boolean loginCliente(CadastroCliente crudC) {
         Scanner scan = new Scanner(System.in);
         System.out.println(":.:.:.:.:.LOGIN.:.:.:.:.:\n\n");
         System.out.println("         CLIENTE         \n");
@@ -28,21 +28,21 @@ public class ShowText {
         String email = scan.next();
         System.out.println("Informe sua senha:");
         String senha = scan.next();
-        return crudC.autentication(email,senha);
+        return crudC.autentication(email, senha);
     }
 
-    public static void menuFuncionario(){
+    public static void menuFuncionario() {
         System.out.println(":.:.:.:.:.MENU PRINCIPAL.:.:.:.:.:\n\n");
         System.out.println("SELECIONE UMA DAS OPÇÕES ABAIXO:");
         System.out.println("1. Visualizar lista de livros");
         System.out.println("2. Cadastrar cliente");
-        System.out.println("4. Deletar esta conta");
-        System.out.println("5. Ir para a gerencia de emprestimos");
-        System.out.println("6. Ir para o gerencia de livros");
-        System.out.println("7. Sair desta conta");
+        System.out.println("3. Deletar esta conta");
+        System.out.println("4. Ir para a gerencia de emprestimos");
+        System.out.println("5. Ir para o gerencia de livros");
+        System.out.println("6. Sair desta conta");
     }
 
-    public static void gerenciaEmprestimo(){
+    public static void gerenciaEmprestimo() {
         System.out.println(":.:.:.:.:.GERÊNCIDA DE EMPRESTIMOS.:.:.:.:.:\n\n");
         System.out.println("SELECIONE UMA DAS OPÇÕES ABAIXO:");
         System.out.println("1. Visualizar emprestimos");
@@ -51,7 +51,7 @@ public class ShowText {
         System.out.println("4. Voltar ao menú principal");
     }
 
-    public static void gerenciaLivros(){
+    public static void gerenciaLivros() {
         System.out.println(":.:.:.:.:.GERENCIA DE LIVROS.:.:.:.:.:\n\n");
         System.out.println("SELECIONE UMA DAS OPÇÕES ABAIXO:");
         System.out.println("1. Cadastrar livro");
@@ -61,7 +61,7 @@ public class ShowText {
         System.out.println("5. Voltar ao menú principal");
     }
 
-    public static void gerenciaAutores(){
+    public static void gerenciaAutores() {
         System.out.println(":.:.:.:.:.GERENCIA DE AUTORES.:.:.:.:.:\n\n");
         System.out.println("SELECIONE UMA DAS OPÇÕES ABAIXO:");
         System.out.println("1. Cadastrar autor");
@@ -70,7 +70,7 @@ public class ShowText {
         System.out.println("4. voltar a genrencia de livros");
     }
 
-    public static void readFuncionario(CadastroFuncionario crudF){
+    public static void readFuncionario(CadastroFuncionario crudF) {
         Scanner scan = new Scanner(System.in);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         System.out.println(":.:.:.:.:.CADASTRO.:.:.:.:.:\n\n");
@@ -86,12 +86,12 @@ public class ShowText {
         System.out.println("Informe sua data de nasicmento:");
         String data = scan.next();
         LocalDate nascimento = LocalDate.parse(data, formatter);
-        Funcionario novo = new Funcionario(email,senha,nome,CPF,nascimento);
+        Funcionario novo = new Funcionario(email, senha, nome, CPF, nascimento);
         crudF.cadastrar(novo);
 
     }
 
-    public static void readCliente(CadastroCliente crudC){
+    public static void readCliente(CadastroCliente crudC) {
         Scanner scan = new Scanner(System.in);
         System.out.println(":.:.:.:.:.CADASTRO.:.:.:.:.:\n\n");
         System.out.println("          CLIENTE           \n");
@@ -103,11 +103,11 @@ public class ShowText {
         String nome = scan.next();
         System.out.println("Informe seu CPF:");
         String CPF = scan.next();
-        Cliente novo = new Cliente(nome,CPF,email,senha);
+        Cliente novo = new Cliente(nome, CPF, email, senha);
         crudC.cadastrar(novo);
     }
 
-    public static void readLivro(CadastroLivro crudL){
+    public static void readLivro(CadastroLivro crudL) {
         Scanner scan = new Scanner(System.in);
         System.out.println(":.:.:.:.:.CADASTRO.:.:.:.:.:\n\n");
         System.out.println("           LIVRO            \n");
@@ -120,32 +120,40 @@ public class ShowText {
         System.out.println("1. ROMANCE\n2. TERROR\n3. POESIA\n4. FANTASIA\n5. AVENTURA\n6. BIOGRAFIA\n7. DIDATICO");
         int choise = scan.nextInt();
         Genero genero;
-        switch (choise){
-            case 1: genero = Genero.ROMANCE;
-                    break;
-            case 2: genero = Genero.TERROR;
-                    break;
-            case 3: genero = Genero.POESIA;
-                    break;
-            case 4: genero = Genero.FANTASIA;
-                    break;
-            case 5: genero = Genero.AVENTURA;
-                    break;
-            case 6: genero = Genero.BIOGRAFIA;
-                    break;
-            case 7: genero = Genero.DIDATICO;
-                    break;
-            default: genero = Genero.ROMANCE;
+        switch (choise) {
+            case 1:
+                genero = Genero.ROMANCE;
+                break;
+            case 2:
+                genero = Genero.TERROR;
+                break;
+            case 3:
+                genero = Genero.POESIA;
+                break;
+            case 4:
+                genero = Genero.FANTASIA;
+                break;
+            case 5:
+                genero = Genero.AVENTURA;
+                break;
+            case 6:
+                genero = Genero.BIOGRAFIA;
+                break;
+            case 7:
+                genero = Genero.DIDATICO;
+                break;
+            default:
+                genero = Genero.ROMANCE;
         }
         Autor autor = null;
         System.out.println("Informe a descrição do livro:");
         String descricao = scan.nextLine();
         scan.nextLine();
-        Livro novo = new Livro(titulo,codigo,genero,autor,descricao);
+        Livro novo = new Livro(titulo, codigo, genero, autor, descricao);
         crudL.cadastrar(novo);
     }
 
-    public static void readAutor(CadastroAutor crudA){
+    public static void readAutor(CadastroAutor crudA) {
         Scanner scan = new Scanner(System.in);
         System.out.println(":.:.:.:.:.CADASTRO.:.:.:.:.:\n\n");
         System.out.println("           AUTOR            \n");
@@ -156,20 +164,28 @@ public class ShowText {
         crudA.cadastrar(autor);
     }
 
-    public static void doEmprestimo(CadastroLivro crudL, CadastroCliente crudC, CadastroEmprestimos crudE){
+    public static void doEmprestimo(CadastroLivro crudL, CadastroCliente crudC, CadastroEmprestimos crudE) {
         Scanner scan = new Scanner(System.in);
         System.out.println(":.:.:.:.:.EMPRESTIMO.:.:.:.:.:\n\n");
         System.out.println("      SELECIONE UM LIVRO      \n");
         System.out.println(crudL);
         int choise = scan.nextInt();
-        Livro escolhido = crudL.consulta(choise-1);
-        System.out.println("Informe o email do cliente:");
-        String email = scan.next();
-        System.out.println("Informe a senha do cliente:");
-        String senha = scan.next();
-        Cliente cliente = crudC.consulta(email,senha);
-        Emprestimo emprestimo = new Emprestimo(escolhido,cliente);
-        System.out.println("A devolução deve ser feita na data: "+ emprestimo.getDataDevolucao());
-        crudE.cadastrar(emprestimo);
+        if (crudL.getEstante()==null){
+            System.out.println("Empréstimo não pode ser realizado, não existem livros!!!");
+
+        }else {
+            Livro escolhido = crudL.consulta(choise - 1);
+            System.out.println("Informe o email do cliente:");
+            String email = scan.next();
+            System.out.println("Informe a senha do cliente:");
+            String senha = scan.next();
+            Cliente cliente = crudC.consulta(email, senha);
+            Emprestimo emprestimo = new Emprestimo(escolhido, cliente);
+            System.out.println("A devolução deve ser feita na data: " + emprestimo.getDataDevolucao());
+            crudE.cadastrar(emprestimo);
+        }
     }
+
 }
+
+
